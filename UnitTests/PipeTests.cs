947e3b44722ee6ce7ef1c63a6fd7b2e6b014524f
busterwood <chris.j.austin@gmail.com>
2016-10-12 15:41:45 +0100

    Add more pipe tests

--- UnitTests/PipeTests.cs
+++ UnitTests/PipeTests.cs
@@ -1,11 +1,8 @@
 ﻿using BusterWood.InputOutput;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Collections.Concurrent;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace UnitTests
 {
@@ -15,19 +12,19 @@ namespace UnitTests
         [Test]
         public void test_single_read_writer_pair()
         {
-            var ends = IO.Pipe();
+            var pipe = IO.Pipe();
             var buf = new Block<byte>(new byte[64]);
             var testData = Encoding.UTF8.GetBytes("hello, world");
             var finished = new ManualResetEventSlim(false);
-            ThreadPool.QueueUserWorkItem(_ => CheckWrite(ends.Writer, testData, finished));
-            var res = ends.Reader.Read(buf);
+            ThreadPool.QueueUserWorkItem(_ => CheckWrite(pipe.Writer, testData, finished));
+            var res = pipe.Reader.Read(buf);
             Assert.IsNull(res.Error);
             Assert.AreEqual(12, res.Bytes);
             var got = Encoding.UTF8.GetString(buf.Array, 0, 12);
             Assert.AreEqual("hello, world", got);
             finished.Wait();
-            ends.Reader.Close();
-            ends.Writer.Close();
+            pipe.Reader.Close();
+            pipe.Writer.Close();
         }
 
         void CheckWrite(IWriter w, byte[] data, ManualResetEventSlim finished)
@@ -36,6 +33,49 @@ namespace UnitTests
             Assert.IsNull(res.Error);
             Assert.AreEqual(data.Length, res.Bytes);
             finished.Set();
+        }
+
+        [Test]
+        public void test_sequence_of_reader_and_writes()
+        {
+            var c = new BlockingCollection<int>();
+            var pipe = IO.Pipe();
+            ThreadPool.QueueUserWorkItem(_ => Reader(pipe.Reader, c));
+            Block<byte> buf = new byte[64];
+            for (var i = 0; i < 5; i++)
+            {
+                var p = buf.Slice(0, 5 + i * 10);
+                var res = pipe.Writer.Write(p);
+
+                Assert.AreEqual(p.Length, res.Bytes, "did not write all bytes");
+                Assert.IsNull(res.Error);
+
+                var nn = c.Take();
+                Assert.AreEqual(res.Bytes, nn, "wrote and read differ");
+            }
+            pipe.Writer.Close();
+            var nLast = c.Take();
+            Assert.AreEqual(0, nLast, "last");
+            pipe.Reader.Close();
+        }
+
+        void Reader(IReader r, BlockingCollection<int> c)
+        {
+            Block<byte> buf = new byte[64];
+            for (;;)
+            {
+                var res = r.Read(buf);
+                if (res.Error == IO.EOF)
+                {
+                    c.Add(0);
+                    break;
+                }
+                if (res.Error != null)
+                {
+                    Assert.Fail("read: %v", res.Error);
+                }
+                c.Add(res.Bytes);
+            }
         }
     }
 }
